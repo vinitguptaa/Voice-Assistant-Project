d4f5404e91ffa64d418b2cc0b4040fa82452546b
vinit <vinitgupta2413@gmail.com>
2026-04-23 11:47:40 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,52 @@
+import speech_recognition as sr 
+import webbrowser
+import pyttsx3
+import musicLibrary
+# pip install pocketsphinx
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")    
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")    
+    elif "open linkedin" in c.lower():
+        webbrowser.open("https://linkedin.com")    
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        link = musicLibrary.music[song]
+        webbrowser.open(link)    
+
+if __name__=="__main__":
+    speak("Initializing Jarvis..")
+    while True:
+
+    # Listen for the wake word "Jarvis"
+    # obtain audio from the microphone
+        r = sr.Recognizer()
+       
+        print("recognizing..")
+        try:
+            with sr.Microphone() as source:
+                print("Listening..")
+                audio = r.listen(source, timeout=2,phrase_time_limit=1)
+
+            word = r.recognize_google(audio)
+            if(word.lower()=="jarvis"):
+                speak("Ya")
+            #Listen for command
+                with sr.Microphone() as source:
+                    print("Listening..")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    processCommand(command)
+        except Exception as e:
+            print("Error; {0}".format(e))
---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "kamleya":"https://www.youtube.com/watch?v=QXJyMpxd210",
+    "kalank":"https://www.youtube.com/watch?v=Grr0FlC8SQA",
+    "ishq":"https://www.youtube.com/watch?v=RzJmBo-2GKI"
+ }
